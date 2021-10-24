1c8cb6c4a364b89161ff3d7c172a9a426adcefb5
ariesrutung <ariesrutung@gmail.com>
2021-10-25 04:44:56 +0900

    update layout

---
+++ application/controllers/Home.php
@@ -0,0 +1,17 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+
+        $data['_view'] = 'public/index';
+        $this->load->view('public/layout', $data);
+    }
+}
